da6fe2a6f75a9ab4229b801e0122a3aa39a3450a
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2019-05-19 19:01:10 +0430

    rsa

---
+++ 00-rsa-algorithm-in-cryptography/src/main/java/Startup.java
@@ -0,0 +1,27 @@
+import cryptography.asymmetric.RSA;
+
+import java.math.BigInteger;
+
+public class Startup {
+    public static void main(String[] args) {
+
+        int N = 1024;
+        RSA key = new RSA(N);
+        System.out.println(key);
+
+        // create random message, encrypt and decrypt
+        // BigInteger message = new BigInteger(N - 1, random);
+
+        // create message by converting string to integer
+        String s = "test";
+        byte[] bytes = s.getBytes();
+        BigInteger message = new BigInteger(bytes);
+
+        BigInteger encrypt = key.encrypt(message);
+        BigInteger decrypt = key.decrypt(encrypt);
+        System.out.println("message   = " + message);
+        System.out.println("encrypted = " + encrypt);
+        System.out.println("decrypted = " + decrypt);
+        System.out.println(new String(decrypt.toByteArray()));
+    }
+}
